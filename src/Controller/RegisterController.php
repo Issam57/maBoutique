a68a90a390f0567b92e6499f1d85c409568f0e3f
Issam57 <issam.boucheraki@gmail.com>
2021-01-10 22:45:30 +0100

    Modification barre navigation

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,7 +4,6 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegisterType;
-use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
